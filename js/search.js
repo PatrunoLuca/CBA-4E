14651871db2d3b8949f6e6bd03c8cbf35ad63be0
GabrieleFerrante <ferrante.gabriele.s25@liceoviconapoli.it>
2023-03-17 22:47:33 +0100

    Added search page and fixed footer
    Added "Dove lo butto" and fixed footer not sticking to the bottom

--- js/search.js
+++ js/search.js
@@ -1,4 +1,39 @@
-$(function () {
-    $('.basicAutoSelect').autoComplete();
-})
+const oggetti = document.querySelector('#oggetti')
+const searchInput = document.querySelector('#search-input')
+const outputContainer = document.querySelector('#output-container')
+const outputParagraph = document.querySelector('#output-paragraph')
 
+let data;
+let searchData = {};
+
+fetch('../assets/objects.json')
+    .then(response => response.json())
+    .then((result) => {
+        data = result
+
+        for (let object of data) {
+            const optionTag = document.createElement('option')
+            optionTag.value = object.item
+
+            oggetti.appendChild(optionTag)
+
+            searchData[object.item.toLowerCase()] = object.bin
+        }
+
+        console.log(searchData)
+
+    })
+
+searchInput.addEventListener('input', (e) => {
+    const input = e.target.value
+    
+    let result = searchData[input.toLowerCase()] ?? null
+
+    if (result != null) {
+        outputParagraph.innerHTML = `Va buttato in: <strong>${result}</strong>`
+        outputContainer.style.visibility = 'visible'
+        
+    } else {
+        outputContainer.style.visibility = 'hidden'
+    }
+})
